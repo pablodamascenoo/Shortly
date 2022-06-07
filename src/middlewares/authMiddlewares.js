2ba443eb49f89660f4b7a67392dba9abb7719e40
pablodamascenoo <pablofacex@gmail.com>
2022-06-07 19:03:45 -0300

    feat: finish signUp middleware

--- src/middlewares/authMiddlewares.js
+++ src/middlewares/authMiddlewares.js
@@ -1,4 +1,5 @@
 import { signInSchema, signUpSchema } from "../schemas/authSchemas.js";
+import db from "../db.js";
 
 export async function checkSignUp(req, res, next) {
     const signUp = req.body;
@@ -7,6 +8,19 @@ export async function checkSignUp(req, res, next) {
 
     if (error) {
         return res.status(422).send(error.details[0].message);
+    }
+
+    try {
+        const user = db.query(`SELECT * FROM users WHERE name=$1 OR email=$2`, [
+            value.name,
+            value.email,
+        ]);
+
+        if (user) {
+            return res.status(409).send("email ou nome já cadastrados");
+        }
+    } catch (e) {
+        return res.status(500).send(e);
     }
 
     res.locals.user = value;
