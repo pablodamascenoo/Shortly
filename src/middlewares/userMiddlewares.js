ebf66cf27faab0ded7a08f11198cdfc45a0e0fec
pablodamascenoo <pablofacex@gmail.com>
2022-06-09 13:20:05 -0300

    feat: added userMiddlewares

---
+++ src/middlewares/userMiddlewares.js
@@ -0,0 +1,22 @@
+import db from "../db.js";
+import { failure } from "../misc/chalkAlerts.js";
+
+export async function findUserId(req, res, next) {
+    const { id } = req.params;
+
+    if (isNaN(parseInt(id)))
+        return res.status(422).send("o id deve ser numérico");
+
+    try {
+        const user = await db.query("SELECT * FROM users WHERE id=$1", [id]);
+
+        if (!user.rowCount)
+            return res.status(404).send("usuário não encontrado");
+
+        res.locals.id = id;
+        next();
+    } catch (error) {
+        failure(error);
+        return res.status(500).send("erro no servidor");
+    }
+}
